989b373b8fdcdb50150d4e737f2eb83f44fc4379
P Vidyashree <vidyashreep2005@gmail.com>
2026-03-05 21:45:10 +0530

    Day07_Longest_Subarray_Majority_Greater_Than_K_Solution

---
+++ Day07_Longest_Subarray_With_Majority_Greater_Than_K.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int longestSubarray(vector<int> &arr, int k) {
+        unordered_map<int,int> mp;
+        mp[0] = -1;
+        int prefixsum = 0, maxlen = 0;
+
+        for(int i = 0; i < arr.size(); i++) {
+            prefixsum += (arr[i] > k) ? 1 : -1;
+
+            if(prefixsum > 0)
+                maxlen = i + 1;
+
+            if(mp.count(prefixsum - 1)) {
+                maxlen = max(maxlen, i - mp[prefixsum - 1]);
+            }
+
+            if(!mp.count(prefixsum))
+                mp[prefixsum] = i;
+        }
+
+        return maxlen;
+    }
+};
