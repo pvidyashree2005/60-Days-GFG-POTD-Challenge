868bb64f06261d43d604902f6a3e8dee3cd6a4f4
P Vidyashree <vidyashreep2005@gmail.com>
2026-03-18 22:47:37 +0530

    Add files via upload

---
+++ Day28_Distribute_Candies_in_Binary_Tree.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <queue>
+#include <sstream>
+#include <cmath>
+using namespace std;
+
+// Tree Node
+/* struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+}; */
+
+// Solution Class
+class Solution {
+    int ans;
+
+public:
+    int distCandy(Node* root) {
+        ans = 0;
+        dfs(root);
+        return ans;
+    }
+
+    int dfs(Node* root) {
+        if (root == nullptr)
+            return 0;
+
+        int l = dfs(root->left);
+        int r = dfs(root->right);
+
+        ans += abs(l) + abs(r);
+
+        return root->data + l + r - 1;
+    }
+};
